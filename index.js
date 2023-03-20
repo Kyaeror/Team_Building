996807146073bffa1b0c692714862481540dd8ff
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-03-19 22:22:31 -0400

    added all functionality and readme

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const fs = require(`fs`)
 const inquirer = require(`inquirer`)
-const createHTML = require(`./Assets/js/createHTML`)
+const addIntern = `
+<div class="col-3" style = "margin: 20px 10px 0; height: 300px;box-shadow: 1px 5px 5px black" >
+            <div class="bg-info">
+                <div class="name h3">Alec</div>
+                <div class="occupation h3">Engineer</div>
+            </div>
+            <div class = "inner-card" style = "margin-left: 20px; box-shadow: 1px 5px 5px black">  
+                <div class="employee-id h6">ID: 2</div>
+                <div class="email h6">Email: <a href= mailto:john@gmail.com target="_blank">john@gmail.com</a></div>
+                <div class="school h6">School: UGA</div> 
+            </div>      
+        </div>
+`
+var HTML = ``
 
 function start () {
 inquirer.prompt([
@@ -25,13 +38,38 @@ inquirer.prompt([
         message: `What is the team manager's office number?`
     },
 ]).then((data)=>{
-    // writeToFile(`./html_test/team.html`, data)
+    HTML += `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Team Building Example</title>
+    </head>
+    <body>
+        <div class="text-center text-white bg-danger h1" style="padding-top: 50px; padding-bottom: 50px;">My Team</div>
+        <div class="row d-flex justify-content-around" style="padding-top: 50px; position: relative; margin-left: 100px; margin-right: 100px;">
+            <!-- manager -->
+            <div class="col-3" style = "margin: 20px 10px 0; height: 300px;box-shadow: 1px 5px 5px black" >
+                <div class="bg-info">
+                    <div class="name h3">${data.name}</div>
+                    <div class="occupation h3">Team Manager</div>
+                </div>
+                <div class = "inner-card" style = "margin-left: 20px; box-shadow: 1px 5px 5px black">  
+                    <div class="employee-id h6">ID: ${data.id}</div>
+                    <div class="email h6">Email: <a href= mailto:${data.email} target="_blank">${data.email}</a></div>
+                    <div class="office-number h6">Office number: ${data.office_number}</div> 
+                </div>      
+            </div>
+    `
     continuePrompt()
 })
 
 //HTML creation
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, createHTML(data), function (err) {
+    fs.writeFile(fileName, HTML, function (err) {
         if (err) {
             return console.log(err)
         }
@@ -71,7 +109,22 @@ function continuePrompt(data){
                     message: `What is the engineer's github?`
                 },
             //loop
-            ]).then((data)=>continuePrompt())
+            ]).then((data)=>{
+                HTML += `
+                <div class="col-3" style = "margin: 20px 10px 0; height: 300px;box-shadow: 1px 5px 5px black" >
+                <div class="bg-info">
+                    <div class="name h3">${data.name}</div>
+                    <div class="occupation h3">Engineer</div>
+                </div>
+                <div class = "inner-card" style = "margin-left: 20px; box-shadow: 1px 5px 5px black">  
+                    <div class="employee-id h6">ID: ${data.id}</div>
+                    <div class="email h6">Email: <a href= mailto:${data.email} target="_blank">${data.email}</a></div>
+                    <div class="github-link h6">Github: <a href="https://github.com/${data.github}" target="_blank">${data.github}</a></div> 
+                </div>      
+                </div>
+                `
+                continuePrompt()
+            })
         }
         if(data.occupation === `Intern`){
             inquirer.prompt([
@@ -93,13 +146,34 @@ function continuePrompt(data){
                 {
                     type: `input`,
                     name: `school`,
-                    message: `What is the intern's github?`
+                    message: `What school does the intern attend?`
                 },
             //loop
-            ]).then((data)=>continuePrompt())
+            ]).then((data)=>{
+                HTML += `
+                <div class="col-3" style = "margin: 20px 10px 0; height: 300px;box-shadow: 1px 5px 5px black" >
+                            <div class="bg-info">
+                                <div class="name h3">${data.name}</div>
+                                <div class="occupation h3">Intern</div>
+                            </div>
+                            <div class = "inner-card" style = "margin-left: 20px; box-shadow: 1px 5px 5px black">  
+                                <div class="employee-id h6">ID: ${data.id}</div>
+                                <div class="email h6">Email: <a href= ${data.email} target="_blank">${data.email}</a></div>
+                                <div class="school h6">School: ${data.school}</div> 
+                            </div>      
+                        </div>
+                `
+                continuePrompt()
+            })
         }
         //exit
         if(data.occupation === `No`){
+            HTML += `
+            </div>
+            </body>
+            </html>
+            `
+            writeToFile(`./html_test/team.html`, data)
             console.log(`Your HTML has been created!`)
         }
     })
